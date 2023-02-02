945790d6072d5cc587c8691880237b54a7ec512e
dahkraf <56779194+dahkraf@users.noreply.github.com>
2023-02-02 20:35:52 +0100

    Delete exercise_5.lean

--- exercise_5.lean
+++
@@ -1,8 +0,0 @@
-/-
-# Exercise 5
-· Explain the difference between the typing judgment typed and the type inferencer type_infer.
-𝔸: ...
-· Explain why the type annotations A and B in lambda abstractions λ(x : A). e and recursive functions rec f (x : A) : B := e are needed for the implementation of your type inferencer. Explain if they are also needed to describe the typing rules.
-𝔸:
--/
-------------
